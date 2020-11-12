a39e226335714948a01b6934a035142946635bfb
cnoriega20 <c79.noriega@gmail.com>
2020-11-12 00:07:09 -0600

    Controller test skeleton

---
+++ src/test/java/com/springboot/rest/controllers/EmployeeControllerTest.java
@@ -0,0 +1,14 @@
+package com.springboot.rest.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmployeeControllerTest {
+
+	@Test
+	public void testGetOne() {
+		fail("Not yet implemented");
+	}
+
+}
