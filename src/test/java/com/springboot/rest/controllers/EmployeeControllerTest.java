eae5a13233c6b28bdf5383a7e97ade245bb54edb
cnoriega20 <c79.noriega@gmail.com>
2020-11-12 22:05:13 -0600

    Changes in test.

--- src/test/java/com/springboot/rest/controllers/EmployeeControllerTest.java
+++ src/test/java/com/springboot/rest/controllers/EmployeeControllerTest.java
@@ -1,10 +1,32 @@
 package com.springboot.rest.controllers;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.rest.SpringbootRestApiApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {SpringbootRestApiApplication.class})
+@WebAppConfiguration
 public class EmployeeControllerTest {
+	
+	private MockLambdaContext lambdaContext;
+	
+	private SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@Test
 	public void testGetOne() {
