c50cd35e531101602c8cc88b64c363616faeb223
cnoriega20 <c79.noriega@gmail.com>
2021-03-19 02:04:23 -0500

    Adding Karate And one feature file for Employee endpoint

---
+++ src/test/java/karate/employee/EmployeeRunner.java
@@ -0,0 +1,11 @@
+package karate.employee;
+
+import com.intuit.karate.junit5.Karate;
+
+public class EmployeeRunner {
+
+    @Karate.Test
+    Karate testAll(){
+        return Karate.run().relativeTo(getClass());
+    }
+}
